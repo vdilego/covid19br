e22f1bd30dbdbaab4733757b4fdd4cfb36a4600b
Wesley Cota <wlcota@gmail.com>
2020-04-05 17:38:10 +0200

    .

--- scrape-covid-saude-gov-br.sh
+++
@@ -1,4 +0,0 @@
-# needs jq -> sudo apt install jq
-curl -s 'https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalMapa' \
--H 'X-Parse-Application-Id: unAFkcaNDeXajurGB7LChj8SgQYS2ptm' \
--H 'TE: Trailers' | jq -r '.results | (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv'
